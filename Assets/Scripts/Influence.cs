f3ab1688b4f62b698defe9a4f7a7cceb2dbb8cff
Merlijn van Nieuwenhoven <merlijnvan11@gmail.com>
2022-09-09 00:26:51 +0200

    Sync between devices
    Commit to push progress from PC to laptop, working on influence range.

--- Assets/Scripts/Influence.cs
+++ Assets/Scripts/Influence.cs
@@ -2,18 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Influence : MonoBehaviour
 {
+
+    private bool isInfluencable = false;
+
+    // Get player prefab
+    public GameObject player;
+
+    private GameObject[] npcs;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // need see all npc close 
-        
+        // Find all NPC prefabs
+        npcs = GameObject.FindGameObjectsWithTag("NPC");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Output distance between player and NPC prefab
+        Debug.Log("Amount of NPC in scene: " + npcs.Length);
+        foreach (GameObject npc in npcs)
+        {
+            // Get index of NPC in npcs
+            
+            float distance = Vector3.Distance(player.transform.position, npc.transform.position);
+
+
+
+            isInfluencable = distance < 5;
+
+            int index = System.Array.IndexOf(npcs, npc);
+            Debug.Log("Index of NPC: " + index + " Distance to player: " + distance + " Influencable: " + isInfluencable);
+
+            
+            
+            
+        }
+
+
+        // If E is pressed, move NPC away from player until distance is greater than 10
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (GameObject npc in npcs)
+            {
+                float distance = Vector3.Distance(player.transform.position, npc.transform.position);
+                if (distance < 10)
+                {
+                    npc.transform.position = Vector3.MoveTowards(npc.transform.position, player.transform.position, 1);
+                }
+            }
+        }
+
+
     }
 }
