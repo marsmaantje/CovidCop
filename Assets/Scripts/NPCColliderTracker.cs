16d4cc657713572d32a7bf8a3a88362d035d477f
marsmaantje[pc] <caspersikkens@planet.nl>
2022-09-19 23:18:06 +0200

    NPC Infection spreads

---
+++ Assets/Scripts/NPCColliderTracker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCColliderTracker : MonoBehaviour
+{
+    public List<NPCBehavior> NPCList { get; private set; } = new List<NPCBehavior>();
+
+    [SerializeField] List<NPCBehavior> IgnoreList = new List<NPCBehavior>();
+
+    public Action<NPCBehavior> NPCEntered;
+    public Action<NPCBehavior> NPCLeft;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NPCBehavior npc = NPCManager.instance.GetNPC(other);
+        if(npc != null && !IgnoreList.Contains(npc))
+        {
+            NPCList.Add(npc);
+            NPCEntered?.Invoke(npc);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NPCBehavior npc = NPCManager.instance.GetNPC(other);
+        if (npc != null && NPCList.Contains(npc) && !IgnoreList.Contains(npc))
+        {
+            NPCList.Remove(npc);
+            NPCLeft?.Invoke(npc);
+        }
+
+    }
+}
