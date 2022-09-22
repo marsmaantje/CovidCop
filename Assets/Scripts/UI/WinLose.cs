93ef632734164b9bf0fc4e508104a8d93b349ce3
merlin <merlijnvan11@gmail.com>
2022-09-22 12:33:50 +0200

    game over

--- Assets/Scripts/UI/WinLose.cs
+++ Assets/Scripts/UI/WinLose.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WinLose : MonoBehaviour
 {
@@ -14,37 +15,62 @@ public class WinLose : MonoBehaviour
     public bool isWin;
     public bool isLose;
 
+    public string sceneName;
+
     Canvas canvas;
 
     void Start()
     {
-        canvas = GetComponent<Canvas>();    
+        canvas = GetComponent<Canvas>();
     }
 
     void Update()
     {
         if (gameOver)
         {
+
+
             Time.timeScale = 0.01f;
             canvas.enabled = true;
             win.enabled = isWin;
             lose.enabled = isLose;
-        } else {
+
+            if (Input.anyKeyDown)
+            //if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))   
+            {
+                if ((!Input.GetMouseButtonDown(0)))
+                {
+                    Time.timeScale = 1f;
+                    SceneManager.LoadScene(sceneName);
+                }
+
+            }
+        }
+        else
+        {
             canvas.enabled = false;
         }
     }
 
-    public void Win() {
+    public void Win()
+    {
         gameOver = true;
         isWin = true;
 
     }
-    public void Lose() {
+    public void Lose()
+    {
         gameOver = true;
         isLose = true;
     }
 
-    // If key is pressed
-    
+
+    public void OnPressed()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
 
 }
