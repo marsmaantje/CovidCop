c73988ce7ba59765d2e20b9cf495a00253d22ae1
merlin <merlijnvan11@gmail.com>
2022-09-22 14:55:50 +0200

    pausehandler

---
+++ Assets/Scripts/UI/PauseHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseHandler : MonoBehaviour
+{
+
+    [SerializeField] private bool isPause = false;
+    [SerializeField] GameObject pauseScreen;
+
+    // Start is called before the first frame update
+
+    
+    public void OnPause(InputAction.CallbackContext callbackContext) {
+        if (callbackContext.performed) {
+            isPause = !isPause;
+            if (isPause) {
+                pauseScreen.SetActive(true);
+                Time.timeScale = 0;
+            } else {
+                pauseScreen.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
